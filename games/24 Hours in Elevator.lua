d5b54aed213181e20d194df586dd4cc8b06ec13f
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-06-26 00:03:16 -0300

    update games/24 Hours in Elevator.lua

--- games/24 Hours in Elevator.lua
+++ games/24 Hours in Elevator.lua
@@ -1,59 +1,52 @@
--- Locals
-local eu = game:GetService("Players").LocalPlayer
-
--- Management Functions
 local function SetESP(instance, color, boolean)
-  if instance:FindFirstChild("Luz") then
-    if instance.Luz.Enabled ~= boolean then
-      instance.Luz.Enabled = boolean
-    end
-  else
+  local luz = instance:FindFirstChild("Luz")
+  if luz then
+    luz.Enabled = boolean
+  elseif boolean then
     local highlight = Instance.new("Highlight")
-    highlight.FillTransparency = 1
-    highlight.Adornee, highlight.Parent = instance, instance
     highlight.Name = "Luz"
-    highlight.FillColor, highlight.OutlineColor = color, color
+    highlight.FillTransparency = 1
+    highlight.FillColor = color
+    highlight.OutlineColor = color
+    highlight.Adornee = instance
+    highlight.Parent = instance
   end
 end
 
--- Control Functions
 local function MobsESP()
+  if not workspace:FindFirstChild("Mobs") then
+    warn("Mobs não encontrado!")
+    return
+  end
+
   while getgenv().MobsESP and task.wait(1) do
     pcall(function()
       for _, mob in pairs(workspace.Mobs:GetChildren()) do
-        SetESP(mob, Color3.FromRGB(255, 125, 0), true)
+        SetESP(mob, Color3.fromRGB(255, 125, 0), true)
       end
       if not workspace.Mobs:GetAttribute("Connected") then
         workspace.Mobs:SetAttribute("Connected", true)
         workspace.Mobs.ChildAdded:Connect(function(obj)
           if getgenv().MobsESP then
-            SetESP(obj, Color3.FromRGB(255, 125, 0), true)
+            SetESP(obj, Color3.fromRGB(255, 125, 0), true)
           end
         end)
       end
     end)
   end
-  if not getgenv().MobsESP then
-    for _, mob in pairs(workspace.Mobs:GetChildren()) do
-      SetESP(mob, 0, false)
-    end
+
+  -- desligar ESP
+  for _, mob in pairs(workspace.Mobs:GetChildren()) do
+    SetESP(mob, Color3.new(), false)
   end
 end
 
--- Tabs
-local Tabs = {
-  Menu = Window:Tab({ Title = "Main", Icon = "house"})
-}
-Window:SelectTab(1)
-
--- Menu
-Tabs.Menu:Section({ Title = "Helpful" })
 Tabs.Menu:Toggle({
   Title = "ESP Mobs",
   Desc = "Highlight mobs.",
   Value = false,
   Callback = function(state)
     getgenv().MobsESP = state
-    MobsESP()
+    coroutine.wrap(MobsESP)()
   end
 })
