278a721e88268c2e0b9d20030d85e88f6467ee6c
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-06-26 13:05:27 -0300

    update games/2 Player Labubu Tycoon.lua

--- games/2 Player Labubu Tycoon.lua
+++ games/2 Player Labubu Tycoon.lua
@@ -1,6 +1,92 @@
+-- Global Values
+getgenv().AutoCollect = false
+getgenv().CollectCoins = false
+
+-- Locals
+local eu = game:GetService("Players").LocalPlayer
+local Settings = {
+  Plot = nil
+}
+
+-- Load
+task.spawn(function()
+  while not Settings.Plot and task.wait(1) do
+    for _, plot in pairs(workspace["Tycoon Kit"].Tycoons:GetChildren()) do
+      if plot:FindFirstChild("Owner") and plot.Owner.Value == eu then
+        Settings.Plot = plot
+        return
+      end
+    end
+  end
+end)
+
+-- Functions
+local function AutoCollect()
+  while getgenv().AutoCollect and task.wait(1) do
+    pcall(function()
+      local CollectPart = Settings.Plot.Essentials.Giver
+      firetouchinterest(eu.Character.HumanoidRootPart, CollectPart, 0)
+      firetouchinterest(eu.Character.HumanoidRootPart, CollectPart, 1)
+    end)
+  end
+end
+local function CollectCoins()
+  while getgenv().CollectCoins and task.wait(1) do
+    pcall(function()
+      for _, coin in pairs(workspace.Coins:GetChildren()) do
+        firetouchinterest(eu.Character.HumanoidRootPart, coin.MeshPart, 0)
+        firetouchinterest(eu.Character.HumanoidRootPart, coin.MeshPart, 1)
+      end
+      if not workspace.Coins:GetAttribute("Connected") then
+        workspace.Coins:SetAttribute("Connected", true)
+        workspace.Coins.ChildAdded:Connect(function(coin)
+          if getgenv().CollectCoins then
+            firetouchinterest(eu.Character.HumanoidRootPart, coin.MeshPart, 0)
+            firetouchinterest(eu.Character.HumanoidRootPart, coin.MeshPart, 1)
+          end
+        end)
+      end
+    end)
+  end
+end
+local function AutoBuy()
+  while getgenv().AutoBuy and task.wait(1) do
+    pcall(function()
+      for _, button in pairs(Settings.Plot.Buttons:GetChildren()) do
+        pcall(function()
+          if not button:FindFirstChild("Gamepass") and button.Head.Transparency == 0 then
+            firetouchinterest(eu.Character.HumanoidRootPart, button.Head, 0)
+            firetouchinterest(eu.Character.HumanoidRootPart, button.Head, 1)
+          end
+        end)
+      end
+    end)
+  end
+end
+
+--[[
 Game: 7750682571 | Place: 133335273076187
 workspace["Tycoon Kit"].Tycoons["Pink Labubu 1"].Owner
 workspace["Tycoon Kit"].Tycoons["Pink Labubu 1"].Buttons["Buy Stairs"].Price
 workspace["Tycoon Kit"].Tycoons["Pink Labubu 1"].Buttons["Buy Stairs"].Head
 workspace["Tycoon Kit"].Tycoons["Pink Labubu 1"].Essentials.Giver.TouchInterest
 workspace.Coins:GetChildren()[3].MeshPart.TouchInterest
+--]]
+
+-- Tabs
+local Tabs = {
+  Menu = Window:Tab({ Title = "Main", Icon = "house"})
+}
+Window:SelectTab(1)
+
+-- Menu
+Tabs.Menu:Section({ Title = "Helpful" })
+Tabs.Menu:Toggle({
+  Title = "Auto Collect",
+  Desc = "Automatically collects cash.",
+  Value = false,
+  Callback = function(state)
+    getgenv().AutoCollect = state
+    AutoCollect()
+  end
+})
