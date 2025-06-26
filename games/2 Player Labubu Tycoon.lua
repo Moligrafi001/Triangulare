7a7063107c44919606f776b0d1e0c3a8a8502a45
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-06-26 12:38:44 -0300

    update games/2 Player Labubu Tycoon.lua

---
+++ games/2 Player Labubu Tycoon.lua
@@ -0,0 +1,6 @@
+Game: 7750682571 | Place: 133335273076187
+workspace["Tycoon Kit"].Tycoons["Pink Labubu 1"].Owner
+workspace["Tycoon Kit"].Tycoons["Pink Labubu 1"].Buttons["Buy Stairs"].Price
+workspace["Tycoon Kit"].Tycoons["Pink Labubu 1"].Buttons["Buy Stairs"].Head
+workspace["Tycoon Kit"].Tycoons["Pink Labubu 1"].Essentials.Giver.TouchInterest
+workspace.Coins:GetChildren()[3].MeshPart.TouchInterest
