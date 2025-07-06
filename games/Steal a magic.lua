867f6db54b9f0fd63b509655eb0912a6269c76ac
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-07-06 15:02:25 -0300

    update games/Steal a magic.lua

---
+++ games/Steal a magic.lua
@@ -0,0 +1,4 @@
+Game: 7911733012 | Place: 84467557068970
+workspace.World.Map.Bases.Base4.Collecters.Collecter1.CollectPrt.TouchInterest
+Owner, IsLocked
+workspace.World.Map.Bases.Base4.LockBase.TouchInterest
