6e7599dc7e01a2815f1e28cc4cd0e577b45a77dc
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-08-10 04:30:39 -0300

    update games/Trap and Bait.lua

--- games/Trap and Bait.lua
+++ games/Trap and Bait.lua
@@ -1,17 +1,76 @@
---[[
-Game: 8169094622 | Place: 117957332897543
-game:GetService("ReplicatedStorage").remotes.client.ui.dialogues.request_sell_all:InvokeServer()
-]]--
-
 -- Global Values
 getgenv().StealAura = false
 getgenv().AutoSell = false
 
 -- Locals
 local eu = game:GetService("Players").LocalPlayer
+local Settings = {
+  Distance = 20
+}
 
 -- Functions
 local function StealAura()
+  local function GetNearby()
+    local Detected = {}
+    for _, prompt in pairs(workspace["__important"].server.baits:GetPartBoundsInBox(eu.Character.HumanoidRootPart.CFrame, Vector3.new(Settings.Distance, 20, Settings.Distance), nil)) do
+      if prompt:IsA("ProximityPrompt") and prompt.ActionText == "Steal" then
+        table.insert(Detected, prompt)
+      end
+    end
+    return Detected
+  end
   while getgenv().StealAura and task.wait(0.1) do
+    if eu:GetAttribute("team") == "rats" then
+      for _, prompt in pairs(GetNearby()) do
+        fireproximityprompt(prompt)
+      end
+    end
   end
 end
+local function AutoSell()
+  while getgenv().AutoSell and task.wait(1) do
+    if eu:GetAttribute("team") == "rats" then
+      game:GetService("ReplicatedStorage").remotes.client.ui.dialogues.request_sell_all:InvokeServer()
+    end
+  end
+end
+
+--[[
+Game: 8169094622 | Place: 117957332897543
+game:GetService("ReplicatedStorage").remotes.client.ui.dialogues.request_sell_all:InvokeServer()
+]]--
+
+-- Tabs
+local Tabs = {
+  Rats = Window:Tab({ Title = "Rats", Icon = "rat"})
+}
+Window:SelectTab(1)
+
+-- Blatant
+Tabs.Rats:Section({ Title = "Steal" })
+Tabs.Rats:Toggle({
+  Title = "Steal Aura",
+  Desc = "Steals all the cheese nearby..",
+  Value = false,
+  Callback = function(state)
+    getgenv().StealAura = state
+    StealAura()
+  end
+})
+Tabs.Rats:Section({ Title = "Sell" })
+Tabs.Rats:Button({
+  Title = "Sell Everything",
+  Desc = "Sells your inventory.",
+  Callback = function()
+    game:GetService("ReplicatedStorage").remotes.client.ui.dialogues.request_sell_all:InvokeServer()
+  end
+})
+Tabs.Rats:Toggle({
+  Title = "Auto Sell",
+  Desc = "Auto sells your cheese.",
+  Value = false,
+  Callback = function(state)
+    getgenv().AutoSell = state
+    AutoSell()
+  end
+})
