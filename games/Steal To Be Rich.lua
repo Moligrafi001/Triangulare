e7e91733797d87c0e8a8f52f71f8b6bfd0dd9d4e
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-08-10 09:13:21 -0300

    update games/Steal To Be Rich.lua

---
+++ games/Steal To Be Rich.lua
@@ -0,0 +1,10 @@
+--[[
+Game: 7778459210 | Place: 133945484297240
+local args = {
+    [1] = "Sales"
+}
+
+game:GetService("ReplicatedStorage").Network.Plot.Collect:InvokeServer(unpack(args))
+game:GetService("ReplicatedStorage").Network.Plot.Lock:FireServer()
+workspace.Map.Plots["2"] Locked Owner
+]]--
