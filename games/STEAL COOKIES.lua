161849191d4b033c6a2383a4315e1a4d3a233246
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-09-05 21:36:47 -0300

    update games/STEAL COOKIES.lua

--- games/STEAL COOKIES.lua
+++ games/STEAL COOKIES.lua
@@ -1,8 +1,12 @@
 -- Global Values
 getgenv().AutoCollect = false
+getgenv().CookieESP = false
 
 -- Locals
 local eu = game:GetService("Players").LocalPlayer
+local Settings = {
+  Busy = false
+}
 
 -- Almost
 local function GetCookies()
@@ -19,34 +23,77 @@ end
 
 -- Functions
 local function CollectCookies()
-  for _, cookie in pairs(GetCookies()) do
-    if (eu.Character.HumanoidRootPart.Position - cookie.WorldPivot.Position).Magnitude <= 10 then
-      fireproximityprompt(cookie.CookiePrompt)
-    end
+  local CookiesList = GetCookies()
+  if Settings.Busy then
+    return WindUI:Notify({
+      Title = "Wait! I'm Busy",
+      Content = "I'm colleting, wait...",
+      Icon = "x",
+      Duration = 5
+    })
+  elseif #GetCookiest == 0 then
+    return WindUI:Notify({
+      Title = "No more cookies.",
+      Content = "Already collected everything.",
+      Icon = "x",
+      Duration = 5
+    })
+  else
+    WindUI:Notify({
+      Title = "Collecting cookies.",
+      Content = "Wait... Almost done.",
+      Icon = "hourglass",
+      Duration = 5
+    })
   end
+  Settings.Busy = true
+  eu:SetAttribute("Triangulare", eu.Character.HumanoidRootPart.CFrame)
+  for _, cookie in pairs(CookiesList) do
+    eu.Character.HumanoidRootPart.CFrame = CFrame.new(cookie.WorldPivot.Position)
+    task.wait(0.1)
+    fireproximityprompt(cookie.CookiePrompt)
+  end
+  eu.Character.HumanoidRootPart.CFrame = eu:GetAttribute("Triangulare")
+  WindUI:Notify({
+    Title = "Perfect! Done!",
+    Content = "Collected all cookies!",
+    Icon = "check",
+    Duration = 5
+  })
+  Settings.Busy = false
 end
 local function AutoCollect()
   while getgenv().AutoCollect and task.wait(0.1) do
     pcall(function()
-      CollectCookies()
+      for _, cookie in pairs(GetCookies()) do
+        if (eu.Character.HumanoidRootPart.Position - cookie.WorldPivot.Position).Magnitude <= 10 then
+          fireproximityprompt(cookie.CookiePrompt)
+        end
+      end
     end)
   end
 end
-local function CookieESP(boolean)
-  for _, cookie in pairs(GetCookies()) do
-    if cookie:FindFirstChild("Highlight") then
-      if cookie.Highlight.Enabled ~= boolean then
-        cookie.Highlight.Enabled = boolean
+local function CookieESP()
+  local function SetESP(state)
+    for _, cookie in pairs(GetCookies()) do
+      if cookie:FindFirstChild("Highlight") then
+        if cookie.Highlight.Enabled ~= boolean then
+          cookie.Highlight.Enabled = boolean
+        end
+      elseif boolean then
+        local highlight = Instance.new("Highlight")
+        highlight.FillColor = Color3.new(1, 0.5, 0)
+        highlight.OutlineColor = Color3.new(1, 0.5, 0)
+        highlight.FillTransparency = 0.7
+        highlight.Adornee = cookie
+        highlight.Parent = cookie
       end
-    elseif boolean then
-      local highlight = Instance.new("Highlight")
-      highlight.FillColor = Color3.new(1, 0.5, 0)
-      highlight.OutlineColor = Color3.new(1, 0.5, 0)
-      highlight.FillTransparency = 0.7
-      highlight.Adornee = cookie
-      highlight.Parent = cookie
     end
   end
+  while getgenv().CookieESP and task.wait(1) do
+    SetESP(true)
+  end
+  if not getgenv().CookieESP then SetESP(false) end
 end
 
 --[[
@@ -68,7 +115,8 @@ Tabs.Menu:Toggle({
   Desc = "Shows you all the cookies.",
   Value = false,
   Callback = function(state)
-    CookieESP(state)
+    getgenv().CookieESP = state
+    CookieESP()
   end
 })
 Tabs.Menu:Toggle({
@@ -78,5 +126,13 @@ Tabs.Menu:Toggle({
   Callback = function(state)
     getgenv().AutoCollect = state
     AutoCollect()
+  end
+})
+Tabs.Menu:Section({ Title = "Blatant" })
+Tabs.Menu:Button({
+  Title = "Collect All",
+  Desc = "Collects all cookies.",
+  Callback = function()
+    CollectCookies()
   end
 })
