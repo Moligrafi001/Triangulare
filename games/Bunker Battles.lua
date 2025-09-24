fbcf315d2eb55cb6b4fb2ac33b2cd4d0d1c6ec7f
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-09-24 16:22:38 -0300

    update games/Bunker Battles.lua

---
+++ games/Bunker Battles.lua
@@ -0,0 +1,8 @@
+local eu = game.Players.LocalPlayer
+while task.wait(0.1) do
+firetouchinterest(eu.Character.HumanoidRootPart, workspace.IMPORTANT.BLOCKS, 0)
+firetouchinterest(eu.Character.HumanoidRootPart, workspace.IMPORTANT.BLOCKS, 1)
+firetouchinterest(eu.Character.HumanoidRootPart, workspace.IMPORTANT.BUNKERS.Blue.TOUCH, 0)
+firetouchinterest(eu.Character.HumanoidRootPart, workspace.IMPORTANT.BUNKERS.Blue.TOUCH, 1)
+end
+Game: 8366180257 | Place: 84767406892643
