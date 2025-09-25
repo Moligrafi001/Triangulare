33db4e8834339429f013c35f35083f7ec4bf778f
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-09-25 06:06:48 -0300

    update games/Rob a Bank.lua

--- games/Rob a Bank.lua
+++ games/Rob a Bank.lua
@@ -1,3 +1,36 @@
+-- Global Values
+getgenv().AutoCollect = false
+getgenv().AutoBeg = false
+
+-- Locals
+local eu = game:GetService("Players").LocalPlayer
+local Settings = {
+  Plot = nil
+}
+
+-- Load
+task.spawn(function()
+  while not Settings.Plot and task.wait(1) do
+    for _, plot in pairs(workspace.Plots:GetChildren()) do
+      if plot:FindFirstChild("Owner") and plot.Owner.Value == eu.Name then
+        Settings.Plot = plot
+        return
+      end
+    end
+  end
+end)
+
+-- Functions
+local function AutoCollect()
+  while getgenv().AutoCollect and task.wait(0.39) do
+    pcall(function()
+      firetouchinterest(eu.Character.HumanoidRootPart, Settings.Plot.MoneyCollecter.Collect, 0)
+      firetouchinterest(eu.Character.HumanoidRootPart, Settings.Plot.MoneyCollecter.Collect, 1)
+    end)
+  end
+end
+
+--[[
 local args = {
     [1] = workspace.Plots.Plot6.Upgrades:FindFirstChild("2")
 }
@@ -8,3 +41,22 @@ workspace.Plots.Plot6.MoneyCollecter.Collect.TouchInterest
 workspace.Plots.Plot6.Owner
 workspace.Plots.Plot6.Upgrades["21"].Price
 workspace.Plots.Plot6.Upgrades["21"].ProximityPrompt
+]]--
+
+-- Tabs
+local Tabs = {
+  Menu = Window:Tab({ Title = "Main", Icon = "house"})
+}
+Window:SelectTab(1)
+
+-- Menu
+Tabs.Menu:Section({ Title = "Helpful" })
+Tabs.Menu:Toggle({
+  Title = "Auto Collect",
+  Desc = "Automatically collects cash.",
+  Value = false,
+  Callback = function(state)
+    getgenv().AutoCollect = state
+    AutoCollect()
+  end
+})
