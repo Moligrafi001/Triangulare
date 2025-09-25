17468c47a4b0955752856bc15928fa849e09ce21
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-09-25 05:01:25 -0300

    update games/Bridge Battles.lua

---
+++ games/Bridge Battles.lua
@@ -0,0 +1,8 @@
+local eu = game.Players.LocalPlayer
+local part = workspace.Classic.Yellow.BlockMain.Giver
+while task.wait(0.1) do
+firetouchinterest(eu.Character.HumanoidRootPart, part, 0)
+firetouchinterest(eu.Character.HumanoidRootPart, part, 1)
+end
+game:GetService("Players").LocalPlayer.BrickCount
+Game: 7960300951 | Place: 87531672335231
