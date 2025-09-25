1e1b95e4d00c4114129b997a098b11071184098c
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-09-25 05:57:33 -0300

    update games/Rob a Bank.lua

---
+++ games/Rob a Bank.lua
@@ -0,0 +1,10 @@
+local args = {
+    [1] = workspace.Plots.Plot6.Upgrades:FindFirstChild("2")
+}
+
+game:GetService("ReplicatedStorage").Events.BuyEvent:FireServer(unpack(args))
+Game: 8319782618 | Place: 107431063731700
+workspace.Plots.Plot6.MoneyCollecter.Collect.TouchInterest
+workspace.Plots.Plot6.Owner
+workspace.Plots.Plot6.Upgrades["21"].Price
+workspace.Plots.Plot6.Upgrades["21"].ProximityPrompt
