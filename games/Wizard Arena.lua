c2c8a1ebc92f1b4024ab53775133e1f2a149ff7e
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-11-23 19:05:02 -0300

    update games/Wizard Arena.lua

---
+++ games/Wizard Arena.lua
@@ -0,0 +1,117 @@
+-- Locals
+local eu = game:GetService("Players").LocalPlayer
+local ReplicatedStorage= game:GetService("ReplicatedStorage")
+local Settings = {
+  Spells = { "Fireball", "Magic Missile", "Rock", "Void", "Shield", "Repair", "Lightning", "Holy Light" },
+  FireRemote = "",
+  Slots = {
+    {
+      "Fireball", "F"
+    },
+    {
+      "Magic Missile", "R"
+    },
+    {
+      "Rock", "G"
+    },
+    {
+      "Void", "V"
+    }
+  }
+}
+
+-- Functions
+task.spawn(function()
+  game:GetService("UserInputService").InputBegan:Connect(function(input, gp)
+    if gp or not Settings.FireRemote or not ReplicatedStorage.ClientRemotes:FindFirstChild(Settings.FireRemote) then return end
+    
+    for _, slot in pairs(Settings.Slots) do
+      if input.KeyCode == Enum.KeyCode[slot[2]] then
+        local camera = workspace.CurrentCamera
+        local rayOrigin = camera.CFrame.Position
+        local rayDirection = camera.CFrame.LookVector * 1000
+        
+        local params = RaycastParams.new()
+        params.FilterDescendantsInstances = { eu.Character }
+        params.FilterType = Enum.RaycastFilterType.Blacklist
+        
+        local result = workspace:Raycast(rayOrigin, rayDirection, params)
+        local hitPosition = result and result.Position or rayOrigin + rayDirection
+        
+        ReplicatedStorage.ClientRemotes:FindFirstChild(Settings.FireRemote):FireServer(slot[1])
+      end
+    end
+  end)
+end)
+
+-- Tabs
+local Tabs = {
+  Menu = Window:Tab({ Title = "Main", Icon = "house"})
+}
+Window:SelectTab(1)
+
+-- Menu
+Tabs.Menu:Section({ Title = "Slot 1" })
+Tabs.Menu:Dropdown({
+  Title = "Selected Spell",
+  Values = Settings.Spells,
+  Value = Settings.Slots[1][1],
+  Callback = function(option)
+    Settings.Slots[1][1] = option
+  end
+})
+Tabs.Menu:Keybind({
+  Title = "Selected Keybind",
+  Value = Settings.Slots[1][2],
+  Callback = function(v)
+    Settings.Slots[1][2] = v
+  end
+})
+Tabs.Menu:Section({ Title = "Slot 2" })
+Tabs.Menu:Dropdown({
+  Title = "Selected Spell",
+  Values = Settings.Spells,
+  Value = Settings.Slots[2][1],
+  Callback = function(option)
+    Settings.Slots[2][1] = option
+  end
+})
+Tabs.Menu:Keybind({
+  Title = "Selected Keybind",
+  Value = Settings.Slots[2][2],
+  Callback = function(v)
+    Settings.Slots[2][2] = v
+  end
+})
+Tabs.Menu:Section({ Title = "Slot 3" })
+Tabs.Menu:Dropdown({
+  Title = "Selected Spell",
+  Values = Settings.Spells,
+  Value = Settings.Slots[3][1],
+  Callback = function(option)
+    Settings.Slots[3][1] = option
+  end
+})
+Tabs.Menu:Keybind({
+  Title = "Selected Keybind",
+  Value = Settings.Slots[3][2],
+  Callback = function(v)
+    Settings.Slots[3][2] = v
+  end
+})
+Tabs.Menu:Section({ Title = "Slot 4" })
+Tabs.Menu:Dropdown({
+  Title = "Selected Spell",
+  Values = Settings.Spells,
+  Value = Settings.Slots[4][1],
+  Callback = function(option)
+    Settings.Slots[4][1] = option
+  end
+})
+Tabs.Menu:Keybind({
+  Title = "Selected Keybind",
+  Value = Settings.Slots[4][2],
+  Callback = function(v)
+    Settings.Slots[4][2] = v
+  end
+})
