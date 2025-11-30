5950af4e1b894d740a29f26c38c37c4742df32ed
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-11-29 22:52:43 -0300

    update games/Last Letter.lua

---
+++ games/Last Letter.lua
@@ -0,0 +1,9 @@
+
+https://api.datamuse.com/words?sp=es*
+workspace.Tables["2"].Billboard.Gui.Starting
+workspace.Tables["8"] Gamemode == "One By One"" or "Last Letter"
+workspace.Tables["2"]["9233473564"]
+local img = game:GetService("Players").LocalPlayer.PlayerGui.Overbar.Frame.Keyboard["2"].A
+game:GetService("Players").LocalPlayer.PlayerGui.Overbar.Frame.Keyboard.Delete
+game:GetService("Players").LocalPlayer.PlayerGui.Overbar.Frame.Keyboard.Done
+firesignal(img.MouseButton1Click)
