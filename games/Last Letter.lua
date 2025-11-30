e191057760314cad460bbcbe032b1b96ffcb5df0
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-11-29 23:33:47 -0300

    update games/Last Letter.lua

--- games/Last Letter.lua
+++ games/Last Letter.lua
@@ -1,4 +1,53 @@
+-- Locals
+local eu = game:GetService("Players").LocalPlayer
+local Settings = {
+  Selected = ""
+}
 
+-- Functions
+local function GetTable()
+  for _, table in pairs(workspace.Tables:GetChildren()) do
+    for _, user in pairs(table:GetChildren()) do
+      if user:IsA("Model") and user.Name == tostring(eu.UserId) then
+        return table
+      end
+    end
+  end
+end
+local function GetLetters()
+  local table = GetTable()
+  if not table then return end
+  local text = Billboard.Gui.Starting.Text
+  
+  return text
+end
+local function PressKey(key)
+  for _, group in pairs(eu.PlayerGui.Overbar.Frame.Keyboard:GetChildren()) do
+    local children = group:GetChildren()
+    if #children >= 3 then
+      for _, k in pairs(children) do
+        if k:IsA("ImageButton") and k.Name == key then
+          return firesignal(k.MouseButton1Click)
+        end
+      end
+    end
+  end
+end
+local function GetWords(letters)
+  local url = "https://api.datamuse.com/words?sp=" .. letters .. "*"
+  local request = game:HttpGet(url, true)
+  local data = game:GetService("HttpService"):JSONDecode(data)
+  
+  local words = {}
+  for i, entry in ipairs(data) do
+    local word = entry.word or ""
+    if word and not table.find(words, word) then
+      table.insert(words, word)
+    end
+  end
+end
+
+--[[
 https://api.datamuse.com/words?sp=es*
 workspace.Tables["2"].Billboard.Gui.Starting
 workspace.Tables["8"] Gamemode == "One By One"" or "Last Letter"
@@ -7,3 +56,46 @@ local img = game:GetService("Players").LocalPlayer.PlayerGui.Overbar.Frame.Keybo
 game:GetService("Players").LocalPlayer.PlayerGui.Overbar.Frame.Keyboard.Delete
 game:GetService("Players").LocalPlayer.PlayerGui.Overbar.Frame.Keyboard.Done
 firesignal(img.MouseButton1Click)
+]]--
+
+-- Tabs
+local Tabs = {
+  Menu = Window:Tab({ Title = "Auto Farm", Icon = "house"})
+}
+Window:SelectTab(1)
+
+-- Menu
+Tabs.Menu:Section({ Title = "Words" })
+local words = Tabs.Menu:Dropdown({
+  Title = "Selected Word",
+  Values = {},
+  Value = Settings.Selected,
+  Callback = function(option)
+    Settings.Selected = option
+  end
+})
+Tabs.Menu:Button({
+  Title = "Refresh List",
+  Desc = "Refreshs the list.",
+  Callback = function()
+    local letras = GetLetters()
+    if not letras then return end
+    
+    words:Refresh(GetWords(letras))
+  end
+})
+Tabs.Menu:Section({ Title = "Type" })
+Tabs.Menu:Button({
+  Title = "Type selected word",
+  Desc = "Types the word you selected.",
+  Callback = function()
+    local letras = GetLetters()
+    if not letras then return end
+    
+    local faltando = Settings.Selected:sub(#letras + 1)
+    
+    for letra in faltando:gmatch(".") do
+      PressKey(letra)
+    end
+  end
+})
