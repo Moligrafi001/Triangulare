059ab9b6b89e50d15772f0a99f41f58973534c9b
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-12-08 03:26:41 -0300

    update extra/Test.lua

--- extra/Test.lua
+++ extra/Test.lua
@@ -1,35 +1,4 @@
-local Luache = loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Luache/main/Source/Library.lua"))()
-Luache:Settings({
-  Service = "triangulare",
-  DebugMode = true
-})
-
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
-WindUI.Services.Luache = {
-  Name = "Luache",
-  Icon = "shield",
-  Args = {"API"},
-  New = function(API)
-    function validateKey(key)
-      if not key then return false, "Key is missing!" end
-      
-      local result = Luache:Check(key)
-      if not result then return false, "Invalid key!" end
-      
-      return true, "Amazing key!"
-    end
-    
-    function copyLink()
-      return setclipboard(Luache:GetKey())
-    end
-    
-    return {
-      Verify = validateKey,
-      Copy = copyLink,
-    }
-  end
-}
-
 local Window = WindUI:CreateWindow({
   Title = "Triangulare | " .. InitializeName,
   Icon = "triangle",
@@ -44,16 +13,12 @@ local Window = WindUI:CreateWindow({
   SideBarWidth = 200,
   HasOutline = true,
 
-  KeySystem = {
-    Note = "Insert your key, or get one.",
-    SaveKey = true,
-    API = {
-      {
-        Type = "Luache",
-        API = "dunno"
-      }
-    }
-  }
+  -- KeySystem = {
+  --   Key = { "iloveyouMoligrafi" },
+  --   Note = "Join our Discord to get the key and unlock the script:",
+  --   URL = "https://discord.gg/9Nmhn8JKjA",
+  --   SaveKey = true,
+  -- },
 })
 Window:EditOpenButton({
   Title = "Triangulare",
