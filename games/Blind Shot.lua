8d22688e18324d66b02b7f3405fc7080606c5c96
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-12-17 15:31:09 -0300

    update games/Blind Shot.lua

--- games/Blind Shot.lua
+++ games/Blind Shot.lua
@@ -1,3 +1,26 @@
+-- Global Values
+getgenv().PlayerESP = false
+getgenv().RayESP = false
+
+-- Locals
+local Players = game:GetService("Players")
+local eu = Players.LocalPlayer
+
+-- Functions
+local function RayESP()
+  while getgenv().RayESP and task.wait(1) do
+    pcall(function()
+      for _, p in pairs(Players:GetPlayers()) do
+        if p ~= eu then
+          local char = p.Character
+          if char and char:FindFirstChild("")
+        end
+      end
+    end)
+  end
+end
+
+--[[
 9277195104/118614517739521
 -- workspace.derekzero0.Skin1.ponto.Beam
 while task.wait(0.1) do
@@ -11,3 +34,22 @@ clone.Enabled = true
 end
 end
 end
+]]--
+
+-- Tabs
+local Tabs = {
+  Menu = Window:Tab({ Title = "Main", Icon = "house"})
+}
+Window:SelectTab(1)
+
+-- Menu
+Tabs.Menu:Section({ Title = "Helpful" })
+Tabs.Menu:Toggle({
+  Title = "Auto Collect",
+  Desc = "Automatically collects blocks.",
+  Value = false,
+  Callback = function(state)
+    getgenv().AutoCollect = state
+    AutoCollect()
+  end
+})
