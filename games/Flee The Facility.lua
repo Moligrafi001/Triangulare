02e98fc35f087bfca418bab12b8f85718c2a7ccd
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2026-01-19 18:15:15 -0300

    update games/Flee The Facility.lua

---
+++ games/Flee The Facility.lua
@@ -0,0 +1,9 @@
+workspace["Shopping Center by D4niel_Foxy"].MirrorModel.TouchDetector.TouchInterest
+workspace["Shopping Center by D4niel_Foxy"].MirrorModel.MirrorFace.ClickDetector
+workspace["Shopping Center by D4niel_Foxy"].Heels.ClickPart.ClickDetector
+
+workspace.Map:GetChildren()[117].DressClone.ClickPart.ClickDetector
+workspace.GameplaySections.Task1.Complete.TouchInterest
+workspace.GameplaySections.Task2.CompleteB.TouchInterest
+workspace.GameplaySections.Task2.CompleteA.TouchInterest
+workspace.Map:GetChildren()[117].DressClone.ClickPart.ClickDetector
