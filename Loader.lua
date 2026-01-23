9e7a9368702f23d0336b2c3fa3ecfa49c7f642fb
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2026-01-23 05:21:16 -0300

    update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -93,6 +93,7 @@ local SupportedGames = {
   [9341457358] = {"games/Save The People.lua", "Save The People"},
   [9363735110] = {"games/Escape Tsunami For Brainrots.lua", "Escape Tsunami For Brainrots"},
   [6938955762] = {"games/Tribe Survival.lua", "Tribe Survival"},
+  [8198700786] = {"games/Anime Playground.lua", "Anime Playground"},
 }
 local Game = SupportedGames[game.GameId] or SupportedGames[game.PlaceId]
 if Game then
