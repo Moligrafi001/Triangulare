2b4b32ffec03eade77e35a9bc8483ae5dfb54aba
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2026-01-25 01:27:52 -0300

    update extra/Initialize.lua

--- extra/Initialize.lua
+++ extra/Initialize.lua
@@ -1,4 +1,3 @@
-local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 WindUI.Services.Luache = {
   Name = "Luache",
   Icon = "shield",
