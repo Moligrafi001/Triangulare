5bacbd325f4a4af31d35f5e0a878b434b74575b4
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2026-07-17 16:47:13 -0300

    update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,2 @@
 -- code moved to a dynamic server sided loader for better performance
-loadstring(game:HttpGet("http://triangulare.xyz/loader?g="..game.GameId.."&p="..game.PlaceId))()
+loadstring(game:HttpGet("https://triangulare.xyz/loader?g="..game.GameId.."&p="..game.PlaceId))()
